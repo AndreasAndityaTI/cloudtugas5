11c0ce966903136caab9ddb68d8fc86022852222
Stefanus <93358497+Stefanus1803@users.noreply.github.com>
2023-12-21 19:54:40 +0700

    Update run_config.sh

--- run_config.sh
+++ run_config.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
@@ -12,12 +11,11 @@ chmod +x docker-compose
 sudo mv docker-compose /usr/bin/
 
 mkdir tugas
-curl -O https://raw.githubusercontent.com/PutuGdeUKDW/CloudProject/main/docker-compose.yml
+curl -O https://raw.githubusercontent.com/Stefanus1803/cloudtugas5/main/docker-compose.yml
 sudo mv docker-compose.yml tugas/
-curl -O https://raw.githubusercontent.com/PutuGdeUKDW/CloudProject/main/nginx.conf
+curl -O https://raw.githubusercontent.com/Stefanus1803/cloudtugas5/main/nginx.conf
 sudo mv nginx.conf tugas/
-curl -O https://raw.githubusercontent.com/PutuGdeUKDW/CloudProject/main/.env
+curl -O https://raw.githubusercontent.com/Stefanus1803/cloudtugas5/main/.env
 sudo mv .env tugas/
 cd tugas/
 docker-compose up -d
-
